b28e75d8c863985f94f0a981f4e1d005f26bbce0
Lynne <ldong1@andrew.cmu.edu>
2018-06-29 13:09:02 -0700

    fixing GetPodByName
    Signed-off-by: Lynne <ldong1@andrew.cmu.edu>

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -1911,7 +1911,10 @@ func (k *k8sOps) listPluginPodsWithOptions(opts meta_v1.ListOptions, plugin stri
 }
 
 func (k *k8sOps) GetPodByName(podName string, namespace string) (*v1.Pod, error) {
-	pod, err := k.client.CoreV1().Pods(namespace).Get(podName, meta_v1.GetOptions{})
+	if err := k.initK8sClient(); err != nil {
+		return nil, err
+	}
+        pod, err := k.client.CoreV1().Pods(namespace).Get(podName, meta_v1.GetOptions{})
 	if err != nil {
 		return nil, ErrPodsNotFound
 	}

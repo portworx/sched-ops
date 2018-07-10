56eea9523c3af767d72a9773bad60d313980672c
Harsh Desai <harsh@portworx.com>
2018-07-10 15:49:19 -0700

    Don't proceed with daemonset validation if observed generation is still 0 (#65)
    * Don't proceed with daemonset validation if observed generation is still 0
    
    0 observed generation would mean the daemonset controller hasn't even started monitoring the daemonset. https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.10/#daemonsetstatus-v1-apps
    
    Signed-off-by: Harsh Desai <harsh@portworx.com>
    
    * address review comments
    
    Signed-off-by: Harsh Desai <harsh@portworx.com>

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -1244,6 +1244,13 @@ func (k *k8sOps) ValidateDaemonSet(name, namespace string, timeout time.Duration
 			return "", true, err
 		}
 
+		if ds.Status.ObservedGeneration == 0 {
+			return "", true, &ErrAppNotReady{
+				ID:    name,
+				Cause: "Observed generation is still 0. Check back status after some time",
+			}
+		}
+
 		pods, err := k.GetDaemonSetPods(ds)
 		if err != nil || pods == nil {
 			return "", true, &ErrAppNotReady{

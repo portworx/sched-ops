09622e0f85222c7f084524a5574ddd0d6cfe84e0
Zoran Rajic <zox@portworx.com>
2022-11-08 22:00:43 -0800

    updating comments
    Signed-off-by: Zoran Rajic <zrajic@purestorage.com>

--- k8s/core/events.go
+++ k8s/core/events.go
@@ -16,7 +16,7 @@ import (
 type EventOps interface {
 	// CreateEvent puts an event into k8s etcd
 	CreateEvent(event *corev1.Event) (*corev1.Event, error)
-	// CreateEvent puts an event into k8s etcd
+	// UpdateEvent updates an event in k8s etcd
 	UpdateEvent(event *corev1.Event) (*corev1.Event, error)
 	// ListEvents retrieves all events registered with kubernetes
 	ListEvents(namespace string, opts metav1.ListOptions) (*corev1.EventList, error)
@@ -30,7 +30,7 @@ func (c *Client) CreateEvent(event *corev1.Event) (*corev1.Event, error) {
 	return c.kubernetes.CoreV1().Events(event.Namespace).Create(context.TODO(), event, metav1.CreateOptions{})
 }
 
-// UpdateEvent updates an event from k8s etcd
+// UpdateEvent updates an event in k8s etcd
 func (c *Client) UpdateEvent(event *corev1.Event) (*corev1.Event, error) {
 	if err := c.initClient(); err != nil {
 		return nil, err

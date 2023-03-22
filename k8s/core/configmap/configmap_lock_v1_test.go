326479785c2fa4161dae0b509ef3e8c73778650c
Nikita Bhatia <nbhatia@purestorage.com>
2023-03-23 00:31:43 +0530

    Fix failing test
    Signed-off-by: Nikita Bhatia <nbhatia@purestorage.com>

--- k8s/core/configmap/configmap_lock_v1_test.go
+++ k8s/core/configmap/configmap_lock_v1_test.go
@@ -112,7 +112,7 @@ func TestLockWithHoldTimeout(t *testing.T) {
 	customHoldTimeout := defaultHoldTimeout + v1DefaultK8sLockRefreshDuration + 10*time.Second
 	fakeClient := fakek8sclient.NewSimpleClientset()
 	coreops.SetInstance(coreops.New(fakeClient))
-	cm, err := New("px-configmaps-test", nil, defaultHoldTimeout, 5, 0, 0)
+	cm, err := New("px-configmaps-test", nil, defaultHoldTimeout, 5, 0, 0, "")
 	require.NoError(t, err, "Unexpected error on New")
 	fmt.Println("TestLockWithHoldTimeout")
 

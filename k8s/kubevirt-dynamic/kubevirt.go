8b927af97423935ae48277aa6d13a8b94bb6fdde
Neelesh Thakur <neelesh.thakur@purestorage.com>
2023-11-07 10:04:43 -0800

    added interface to get or list virtualMachines to kubevirt-dynamic (#339)
    Also, added uid of owner VM to the vmi
    
    Signed-off-by: Neelesh Thakur <neelesh.thakur@purestorage.com>

--- k8s/kubevirt-dynamic/kubevirt.go
+++ k8s/kubevirt-dynamic/kubevirt.go
@@ -21,6 +21,7 @@ var (
 
 // Ops is an interface to perform generic Object operations
 type Ops interface {
+	VirtualMachineOps
 	VirtualMachineInstanceOps
 	VirtualMachineInstanceMigrationOps
 	// SetConfig sets the config and resets the client

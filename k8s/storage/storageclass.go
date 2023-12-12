8fe91a0c4a2d5c41692330a9d2605d6cf82c30dc
Mithun Koppal <mkoppal@purestorage.com>
2023-12-12 17:56:40 +0530

    Added a method GetAllStorageClasses
    Signed-off-by: Mithun Koppal <mkoppal@purestorage.com>

--- k8s/storage/storageclass.go
+++ k8s/storage/storageclass.go
@@ -10,9 +10,11 @@ import (
 
 // ScOps is an interface to perform k8s storage class operations
 type ScOps interface {
+	// GetAllStorageClasses returns all storageClasses
+	GetAllStorageClasses() (*storagev1.StorageClassList, error)
 	// GetStorageClasses returns all storageClasses that match given optional label selector
 	GetStorageClasses(labelSelector map[string]string) (*storagev1.StorageClassList, error)
-	// GetStorageClass returns the storage class for the give namme
+	// GetStorageClass returns the storage class for the give name
 	GetStorageClass(name string) (*storagev1.StorageClass, error)
 	// GetDefaultStorageClasses returns all storageClasses that are set as default
 	GetDefaultStorageClasses() (*storagev1.StorageClassList, error)
@@ -127,4 +129,12 @@ func (c *Client) AnnotateStorageClassAsDefault(name string) error {
 		return err
 	}
 	return nil
+}
+
+// GetAllStorageClasses returns all storageClasses
+func (c *Client) GetAllStorageClasses() (*storagev1.StorageClassList, error) {
+	if err := c.initClient(); err != nil {
+		return nil, err
+	}
+	return c.storage.StorageClasses().List(context.TODO(), metav1.ListOptions{})
 }

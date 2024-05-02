4353155f7b44e6240c2fb99c0fd7e57b78894638
Neelesh Thakur <neelesh.thakur@purestorage.com>
2024-05-01 17:22:09 -0700

    fixup! PWX-34271: migration with params (#377) (#379)
    Signed-off-by: Neelesh Thakur <neelesh.thakur@purestorage.com>

--- k8s/kubevirt-dynamic/virtualmachineinstancemigration.go
+++ k8s/kubevirt-dynamic/virtualmachineinstancemigration.go
@@ -52,7 +52,7 @@ type VirtualMachineInstanceMigrationOps interface {
 
 	// CreateVirtualMachineInstanceMigrationWithParams starts live migration of the specified VMI
 	CreateVirtualMachineInstanceMigrationWithParams(vmiNamespace, vmiName string,
-		migrationNam, generateNamePrefix string, annotations, labels map[string]string) (*VirtualMachineInstanceMigration, error)
+		migrationName, generateNamePrefix string, annotations, labels map[string]string) (*VirtualMachineInstanceMigration, error)
 
 	// GetVirtualMachineInstanceMigration retrieves some info about the specified VMI
 	GetVirtualMachineInstanceMigration(namespace, name string) (*VirtualMachineInstanceMigration, error)

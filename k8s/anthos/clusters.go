21ac3db63d2c343a7644be133545f543163f6a48
snigdha-px <sagrawal@purestorage.com>
2024-06-13 19:38:44 +0530

    Latest pkg for PSA

--- k8s/anthos/clusters.go
+++ k8s/anthos/clusters.go
@@ -5,7 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 
-	v1alpha1 "sigs.k8s.io/cluster-api/pkg/apis/deprecated/v1alpha1"
+	//v1alpha1 "sigs.k8s.io/cluster-api/pkg/apis/deprecated/v1alpha1"
+	v1alpha1 "sigs.k8s.io/cluster-api/api/v1beta1"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"

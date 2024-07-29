acc927b7ffc3521ba94a094015a8b8443f31564d
Jose Rivera <jose@portworx.com>
2024-07-29 23:45:28 +0000

    PWX-27452: Add k8s/core/configmap from 'master' sched-ops to support go.mod migration.
    Signed-off-by: Jose Rivera <jose@portworx.com>

---
+++ k8s/core/configmap/configmap_lock_v1_test.go
@@ -0,0 +1,143 @@
+package configmap
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	coreops "github.com/portworx/sched-ops/k8s/core"
+	"github.com/stretchr/testify/require"
+	fakek8sclient "k8s.io/client-go/kubernetes/fake"
+)
+
+func TestLock(t *testing.T) {
+	fakeClient := fakek8sclient.NewSimpleClientset()
+	coreops.SetInstance(coreops.New(fakeClient))
+	cm, err := New("px-configmaps-test", nil, lockTimeout, 5, 0, 0)
+	require.NoError(t, err, "Unexpected error on New")
+	fmt.Println("testLock")
+
+	id := "locktest"
+	err = cm.Lock(id)
+	require.NoError(t, err, "Unexpected error in lock")
+
+	fmt.Println("\tunlock")
+	err = cm.Unlock()
+	require.NoError(t, err, "Unexpected error from Unlock")
+
+	fmt.Println("\trelock")
+	err = cm.Lock(id)
+	require.NoError(t, err, "Failed to lock after unlock")
+
+	fmt.Println("\treunlock")
+	err = cm.Unlock()
+	require.NoError(t, err, "Unexpected error from Unlock")
+
+	fmt.Println("\trepeat lock once")
+	err = cm.Lock(id)
+	require.NoError(t, err, "Failed to lock unlock")
+
+	done := 0
+	go func() {
+		time.Sleep(time.Second * 3)
+		done = 1
+		err := cm.Unlock()
+		fmt.Println("\trepeat lock unlock once")
+		require.NoError(t, err, "Unexpected error from Unlock")
+	}()
+	fmt.Println("\trepeat lock lock twice")
+	err = cm.Lock(id)
+	require.NoError(t, err, "Failed to lock")
+	require.Equal(t, 1, done, "Locked before unlock")
+	fmt.Println("\trepeat lock unlock twice")
+	err = cm.Unlock()
+	require.NoError(t, err, "Unexpected error from Unlock")
+
+	for done == 0 {
+		time.Sleep(time.Second)
+	}
+
+	id = "doubleLock"
+	err = cm.Lock(id)
+	require.NoError(t, err, "Unexpected error in lock")
+	go func() {
+		time.Sleep(3 * time.Second)
+		err := cm.Unlock()
+		require.NoError(t, err, "Unexpected error from Unlock")
+	}()
+	err = cm.Lock(id)
+	require.NoError(t, err, "Double lock")
+	err = cm.Unlock()
+	require.NoError(t, err, "Unexpected error from Unlock")
+
+	err = cm.Lock("id1")
+	require.NoError(t, err, "Unexpected error in lock")
+	go func() {
+		time.Sleep(1 * time.Second)
+		err := cm.Unlock()
+		require.NoError(t, err, "Unexpected error from Unlock")
+	}()
+	err = cm.Lock("id2")
+	require.NoError(t, err, "diff lock")
+	err = cm.Unlock()
+	require.NoError(t, err, "Unexpected error from Unlock")
+
+	fmt.Println("\tlockExpiration")
+
+	var lockTimedout bool
+	fatalLockCb := func(format string, args ...interface{}) {
+		fmt.Println("\tLock timeout called.")
+		lockTimedout = true
+	}
+	SetFatalCb(fatalLockCb)
+
+	err = cm.Lock("id2")
+	require.NoError(t, err, "Unexpected error in lock")
+	time.Sleep(20 * time.Second)
+	require.True(t, lockTimedout, "Lock hold timeout not triggered")
+
+	err = cm.Unlock()
+	require.NoError(t, err, "Unexpected no error in unlock")
+
+	err = cm.Lock("id3")
+	require.NoError(t, err, "Lock should have expired")
+	err = cm.Unlock()
+	require.NoError(t, err, "Unexpected no error in unlock")
+	err = cm.Delete()
+	require.NoError(t, err, "Unexpected error on Delete")
+}
+
+func TestLockWithHoldTimeout(t *testing.T) {
+	defaultHoldTimeout := 3 * time.Second
+	customHoldTimeout := defaultHoldTimeout + v1DefaultK8sLockRefreshDuration + 10*time.Second
+	fakeClient := fakek8sclient.NewSimpleClientset()
+	coreops.SetInstance(coreops.New(fakeClient))
+	cm, err := New("px-configmaps-test", nil, defaultHoldTimeout, 5, 0, 0)
+	require.NoError(t, err, "Unexpected error on New")
+	fmt.Println("TestLockWithHoldTimeout")
+
+	var lockTimedout bool
+	fatalLockCb := func(format string, args ...interface{}) {
+		fmt.Println("\tLock timeout called.")
+		lockTimedout = true
+	}
+	SetFatalCb(fatalLockCb)
+
+	// when custom lock hold timeout is more than the default lock hold timeout
+	err = cm.LockWithParams("id1", customHoldTimeout, 0)
+	require.NoError(t, err, "Unexpected error in lock")
+	time.Sleep(20 * time.Second)
+
+	err = cm.Unlock()
+	require.NoError(t, err, "Unexpected no error in unlock")
+
+	// lock hold timeout should not trigger after the default lock hold timeout period (plus refresh interval)
+	time.Sleep(customHoldTimeout - 8*time.Second)
+	require.False(t, lockTimedout, "Lock hold timeout should not have triggered")
+
+	err = cm.Unlock()
+	require.NoError(t, err, "Unexpected no error in unlock")
+
+	err = cm.Delete()
+	require.NoError(t, err, "Unexpected error on Delete")
+}
---
+++ k8s/core/configmap/configmap_lock_v2_test.go
@@ -0,0 +1,140 @@
+package configmap
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	coreops "github.com/portworx/sched-ops/k8s/core"
+	"github.com/stretchr/testify/require"
+	fakek8sclient "k8s.io/client-go/kubernetes/fake"
+)
+
+const (
+	lockTimeout = 15 * time.Second
+)
+
+func TestMultilock(t *testing.T) {
+	t.Skip("skipped until PWX-31627 is fixed")
+	fakeClient := fakek8sclient.NewSimpleClientset()
+	coreops.SetInstance(coreops.New(fakeClient))
+	cm, err := New("px-configmaps-test", nil, lockTimeout, 3, 0, 0)
+	require.NoError(t, err, "Unexpected error on New")
+
+	fmt.Println("testMultilock")
+
+	id1 := "id1"
+	id2 := "id2"
+	key1 := "key1"
+	key2 := "key2"
+
+	locked, _, err := cm.IsKeyLocked(key1)
+	require.NoError(t, err)
+	require.False(t, locked)
+
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+
+	locked, owner, err := cm.IsKeyLocked(key1)
+	require.NoError(t, err)
+	require.True(t, locked)
+	require.Equal(t, id1, owner)
+
+	fmt.Println("\tlocking key 2")
+	err = cm.LockWithKey(id2, key2)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id2,key2)")
+
+	locked, owner, err = cm.IsKeyLocked(key2)
+	require.NoError(t, err)
+	require.True(t, locked)
+	require.Equal(t, id2, owner)
+
+	fmt.Println("\tunlocking key 1")
+	err = cm.UnlockWithKey(key1)
+	require.NoError(t, err, "Unexpected error in UnlockWithKey(id1,key1)")
+
+	locked, owner, err = cm.IsKeyLocked(key1)
+	require.NoError(t, err)
+	require.False(t, locked)
+
+	fmt.Println("\tunlocking key 2")
+	err = cm.UnlockWithKey(key2)
+	require.NoError(t, err, "Unexpected error in UnlockWithKey(id1,key1)")
+
+	locked, owner, err = cm.IsKeyLocked(key2)
+	require.NoError(t, err)
+	require.False(t, locked)
+
+	fmt.Println("\tdouble lock with same id")
+	fmt.Println("\tlocking ID 1 key 1")
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		// Second lock below will block until this unlock (because of same ID)
+		fmt.Println("\tunlocking key 1")
+		err := cm.UnlockWithKey(key1)
+		require.NoError(t, err, "Unexpected error from UnlockWithKey(key1)")
+	}()
+	fmt.Println("\tlocking ID 1 key 2")
+	err = cm.LockWithKey(id1, key2)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key2)")
+
+	fmt.Println("\tunlocking key 2")
+	err = cm.UnlockWithKey(key2)
+	require.NoError(t, err, "Unexpected error in UnlockWithKey(key2)")
+
+	fmt.Println("\tdouble lock with same key")
+	fmt.Println("\tlocking ID 1 key 1")
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		// Second lock below will block until this unlock (because of same key)
+		fmt.Println("\tunlocking key 1")
+		err := cm.UnlockWithKey(key1)
+		require.NoError(t, err, "Unexpected error from UnlockWithKey(key1)")
+	}()
+	fmt.Println("\tlocking ID 2 key 1")
+	err = cm.LockWithKey(id2, key1)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id2,key1)")
+
+	fmt.Println("\tunlocking key 1")
+	err = cm.UnlockWithKey(key1)
+	require.NoError(t, err, "Unexpected error in UnlockWithKey(key1)")
+
+	fmt.Println("\tlockExpiration")
+
+	var lockTimedout bool
+	fatalLockCb := func(format string, args ...interface{}) {
+		fmt.Println("\tLock timeout called.")
+		lockTimedout = true
+	}
+	SetFatalCb(fatalLockCb)
+
+	// Check lock expiration
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in lock")
+
+	// Locking again with same owner should not throw error
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in lock")
+	time.Sleep((v2DefaultK8sLockRefreshDuration * 3) + (3 * time.Second))
+	require.True(t, lockTimedout, "Lock hold timeout not triggered")
+
+	// Locking again with expired lock should not throw error
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in lock")
+
+	err = cm.UnlockWithKey(key1)
+	require.NoError(t, err, "Unexpected no error in unlock")
+
+	err = cm.LockWithKey(id2, key1)
+	require.NoError(t, err, "Lock should have expired")
+	err = cm.UnlockWithKey(key1)
+	require.NoError(t, err, "Unexpected no error in unlock")
+	err = cm.Delete()
+	require.NoError(t, err, "Unexpected error on Delete")
+}

035ca31c7dfd8a7df7975c5a398f0972c8999e64
Neelesh Thakur <neelesh.thakur@purestorage.com>
2024-08-07 11:43:18 -0600

    PWX-38340: fix race issues when updating k8s configmaps
    Problems that this patch tries to fix:
    1. Some callers updated the keys that they had not locked because they specified all keys to Patch()
    2. Update() was dangerous because it could overwrite the internal keys used to store locks
    3. RefreshLock() could lose the lock and re-acquire it siliently thereby overwriting
       the intervening update made by someone else
    4. Patch could update the configmap while RefreshLock() has lost the ownership of the lock
    5. V2 locks were re-entrant but did not keep the reference count causing premature
       unlocking
    
    Changes:
    1. Get rid of the configmap Update() method since it is too dangerous
    2. Refactor Patch() into PatchKeyLocked() that updates a single key after verifying lock owner
    3. RefreshLock detects loss of lock ownership
    4. Add DeleteKeyLocked() method so that a key can be deleted safely
    5. Add a generation number to the configmap and log each time the generation is incremented
    6. Make v2 locks non-reentrant (like v1 locks) and fix one caller that was expecting the lock
       to be re-entrant
    
    Signed-off-by: Neelesh Thakur <neelesh.thakur@purestorage.com>

--- k8s/core/configmap/configmap_test.go
+++ k8s/core/configmap/configmap_test.go
@@ -17,7 +17,7 @@ func TestGetConfigMap(t *testing.T) {
 	configData := map[string]string{
 		"key1": "val1",
 	}
-	cm, err := New("px-configmaps-test", configData, lockTimeout, 5, 0, 0)
+	cm, err := New("px-configmaps-test", configData, testLockTimeout, 5, 0, 0)
 	require.NoError(t, err, "Unexpected error in creating configmap")
 
 	resultMap, err := cm.Get()
@@ -34,56 +34,10 @@ func TestDeleteConfigMap(t *testing.T) {
 		"key1": "val1",
 	}
 
-	cm, err := New("px-configmaps-test", configData, lockTimeout, 5, 0, 0)
+	cm, err := New("px-configmaps-test", configData, testLockTimeout, 5, 0, 0)
 	require.NoError(t, err, "Unexpected error in creating configmap")
 
 	err = cm.Delete()
 	require.NoError(t, err, "Unexpected error in delete")
 
-}
-
-func TestPatchConfigMap(t *testing.T) {
-	fakeClient := fakek8sclient.NewSimpleClientset()
-	coreops.SetInstance(coreops.New(fakeClient))
-
-	configData := map[string]string{
-		"key1": "val1",
-	}
-
-	cm, err := New("px-configmaps-test", configData, lockTimeout, 5, 0, 0)
-	require.NoError(t, err, "Unexpected error in creating configmap")
-
-	dummyData := map[string]string{
-		"key2": "val2",
-	}
-
-	err = cm.Patch(dummyData)
-	require.NoError(t, err, "Unexpected error in Patch")
-	resultMap, err := cm.Get()
-	require.Contains(t, resultMap, "key1")
-	require.Contains(t, resultMap, "key2")
-	fmt.Println(resultMap)
-}
-
-func TestUpdateConfigMap(t *testing.T) {
-	fakeClient := fakek8sclient.NewSimpleClientset()
-	coreops.SetInstance(coreops.New(fakeClient))
-
-	configData := map[string]string{
-		"key1": "val1",
-	}
-
-	cm, err := New("px-configmaps-test", configData, lockTimeout, 5, 0, 0)
-	require.NoError(t, err, "Unexpected error in creating configmap")
-
-	dummyData := map[string]string{
-		"key2": "val2",
-	}
-
-	err = cm.Update(dummyData)
-	require.NoError(t, err, "Unexpected error in Update")
-	resultMap, err := cm.Get()
-	require.NotContains(t, resultMap, "key1")
-	require.Contains(t, resultMap, "key2")
-	fmt.Println(resultMap)
 }

948bf116243ec56bd041367356d45c3fd919a14c
Neelesh Thakur <neelesh.thakur@purestorage.com>
2024-08-12 16:43:04 -0700

    PWX-38340: fix race issues when updating k8s configmaps (#403)
    * PWX-38340: fix race issues when updating k8s configmaps
    
    Problems that this patch tries to fix:
    1. Some callers updated the keys that they had not locked because they specified all keys to Patch()
    2. Update() was dangerous because it could overwrite the internal keys used to store locks
    3. RefreshLock() could lose the lock and re-acquire it siliently thereby overwriting
       the intervening update made by someone else
    4. Patch could update the configmap while RefreshLock() has lost the ownership of the lock
    5. V2 locks were re-entrant but did not keep the reference count causing premature
       unlocking
    
    Changes:
    1. Get rid of the configmap Update() method since it is too dangerous
    2. Refactor Patch() into PatchKeyLocked() that updates a single key after verifying lock owner
    3. RefreshLock detects loss of lock ownership
    4. Add DeleteKeyLocked() method so that a key can be deleted safely
    5. Add a generation number to the configmap and log each time the generation is incremented
    6. Make v2 locks non-reentrant (like v1 locks)
    
    Signed-off-by: Neelesh Thakur <neelesh.thakur@purestorage.com>

--- k8s/core/configmap/configmap_lock_v2_test.go
+++ k8s/core/configmap/configmap_lock_v2_test.go
@@ -11,14 +11,16 @@ import (
 )
 
 const (
-	lockTimeout = 15 * time.Second
+	testLockTimeout         = 5 * time.Second
+	testLockAttempts        = 3
+	testLockRefreshDuration = 1 * time.Second
+	testLockTTL             = 3 * time.Second
 )
 
 func TestMultilock(t *testing.T) {
-	t.Skip("skipped until PWX-31627 is fixed")
 	fakeClient := fakek8sclient.NewSimpleClientset()
 	coreops.SetInstance(coreops.New(fakeClient))
-	cm, err := New("px-configmaps-test", nil, lockTimeout, 3, 0, 0)
+	cm, err := New("px-configmaps-test", nil, testLockTimeout, testLockAttempts, testLockRefreshDuration, testLockTTL)
 	require.NoError(t, err, "Unexpected error on New")
 
 	fmt.Println("testMultilock")
@@ -32,8 +34,10 @@ func TestMultilock(t *testing.T) {
 	require.NoError(t, err)
 	require.False(t, locked)
 
+	fmt.Println("\tlocking key 1")
 	err = cm.LockWithKey(id1, key1)
 	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+	fmt.Println("\tlocked ID 1 key 1")
 
 	locked, owner, err := cm.IsKeyLocked(key1)
 	require.NoError(t, err)
@@ -43,6 +47,7 @@ func TestMultilock(t *testing.T) {
 	fmt.Println("\tlocking key 2")
 	err = cm.LockWithKey(id2, key2)
 	require.NoError(t, err, "Unexpected error in LockWithKey(id2,key2)")
+	fmt.Println("\tlocked ID 2 key 2")
 
 	locked, owner, err = cm.IsKeyLocked(key2)
 	require.NoError(t, err)
@@ -52,16 +57,18 @@ func TestMultilock(t *testing.T) {
 	fmt.Println("\tunlocking key 1")
 	err = cm.UnlockWithKey(key1)
 	require.NoError(t, err, "Unexpected error in UnlockWithKey(id1,key1)")
+	fmt.Println("\tunlocked ID 1 key 1")
 
-	locked, owner, err = cm.IsKeyLocked(key1)
+	locked, _, err = cm.IsKeyLocked(key1)
 	require.NoError(t, err)
 	require.False(t, locked)
 
 	fmt.Println("\tunlocking key 2")
 	err = cm.UnlockWithKey(key2)
 	require.NoError(t, err, "Unexpected error in UnlockWithKey(id1,key1)")
+	fmt.Println("\tunlocked ID 2 key 2")
 
-	locked, owner, err = cm.IsKeyLocked(key2)
+	locked, _, err = cm.IsKeyLocked(key2)
 	require.NoError(t, err)
 	require.False(t, locked)
 
@@ -69,72 +76,234 @@ func TestMultilock(t *testing.T) {
 	fmt.Println("\tlocking ID 1 key 1")
 	err = cm.LockWithKey(id1, key1)
 	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+	fmt.Println("\tlocked ID 1 key 1")
 
 	go func() {
 		time.Sleep(1 * time.Second)
 		// Second lock below will block until this unlock (because of same ID)
-		fmt.Println("\tunlocking key 1")
+		fmt.Println("\tunlocking ID 1 key 1")
 		err := cm.UnlockWithKey(key1)
 		require.NoError(t, err, "Unexpected error from UnlockWithKey(key1)")
+		fmt.Println("\tunlocked ID 1 key 1")
 	}()
 	fmt.Println("\tlocking ID 1 key 2")
 	err = cm.LockWithKey(id1, key2)
 	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key2)")
+	fmt.Println("\tlocked ID 1 key 2")
 
 	fmt.Println("\tunlocking key 2")
 	err = cm.UnlockWithKey(key2)
 	require.NoError(t, err, "Unexpected error in UnlockWithKey(key2)")
+	fmt.Println("\tunlocked key 2")
 
 	fmt.Println("\tdouble lock with same key")
 	fmt.Println("\tlocking ID 1 key 1")
 	err = cm.LockWithKey(id1, key1)
 	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+	fmt.Println("\tlocked ID 1 key 1")
 
 	go func() {
 		time.Sleep(1 * time.Second)
 		// Second lock below will block until this unlock (because of same key)
-		fmt.Println("\tunlocking key 1")
+		fmt.Println("\tunlocking ID 1 key 1")
 		err := cm.UnlockWithKey(key1)
 		require.NoError(t, err, "Unexpected error from UnlockWithKey(key1)")
+		fmt.Println("\tunlocked ID 1 key 1")
 	}()
 	fmt.Println("\tlocking ID 2 key 1")
 	err = cm.LockWithKey(id2, key1)
 	require.NoError(t, err, "Unexpected error in LockWithKey(id2,key1)")
+	fmt.Println("\tlocked ID 2 key 1")
 
 	fmt.Println("\tunlocking key 1")
 	err = cm.UnlockWithKey(key1)
 	require.NoError(t, err, "Unexpected error in UnlockWithKey(key1)")
+	fmt.Println("\tunlocked ID 2 key 1")
 
-	fmt.Println("\tlockExpiration")
+	// all keys should be unlocked now
+	locked, _, err = cm.IsKeyLocked(key1)
+	require.NoError(t, err)
+	require.False(t, locked)
+
+	locked, _, err = cm.IsKeyLocked(key2)
+	require.NoError(t, err)
+	require.False(t, locked)
+
+	err = cm.Delete()
+	require.NoError(t, err, "Unexpected error on Delete")
+}
+
+func TestLockHoldTimeout(t *testing.T) {
+	fakeClient := fakek8sclient.NewSimpleClientset()
+	coreops.SetInstance(coreops.New(fakeClient))
+	cm, err := New("px-configmaps-test", nil, testLockTimeout, testLockAttempts, testLockRefreshDuration, testLockTTL)
+	require.NoError(t, err, "Unexpected error on New")
+
+	fmt.Println("TestLockHoldTimeout")
+
+	id1 := "id1"
+	key1 := "key1"
+	id2 := "id2"
 
 	var lockTimedout bool
 	fatalLockCb := func(format string, args ...interface{}) {
 		fmt.Println("\tLock timeout called.")
 		lockTimedout = true
+		// since our fatalCb does not panic, we need to unlock to stop the lock from refreshing
+		go func() {
+			fmt.Println("\tunlocking key 1 from fatalCb")
+			cm.UnlockWithKey(key1)
+			fmt.Println("\tunlocked key 1 from fatalCb")
+		}()
 	}
 	SetFatalCb(fatalLockCb)
 
 	// Check lock expiration
+	fmt.Println("\tlocking ID 1 key 1")
 	err = cm.LockWithKey(id1, key1)
 	require.NoError(t, err, "Unexpected error in lock")
+	fmt.Println("\tlocked ID 1 key 1")
 
-	// Locking again with same owner should not throw error
-	err = cm.LockWithKey(id1, key1)
-	require.NoError(t, err, "Unexpected error in lock")
-	time.Sleep((v2DefaultK8sLockRefreshDuration * 3) + (3 * time.Second))
+	// Wait for lock hold timeout which will stop the refresh
+	time.Sleep(testLockTimeout + time.Second)
 	require.True(t, lockTimedout, "Lock hold timeout not triggered")
 
-	// Locking again with expired lock should not throw error
+	// Locking again should not throw error
 	err = cm.LockWithKey(id1, key1)
 	require.NoError(t, err, "Unexpected error in lock")
 
 	err = cm.UnlockWithKey(key1)
-	require.NoError(t, err, "Unexpected no error in unlock")
+	require.NoError(t, err, "Unexpected error in unlock")
 
 	err = cm.LockWithKey(id2, key1)
-	require.NoError(t, err, "Lock should have expired")
+	require.NoError(t, err, "Unexpected error in lock")
 	err = cm.UnlockWithKey(key1)
-	require.NoError(t, err, "Unexpected no error in unlock")
+	require.NoError(t, err, "Unexpected error in unlock")
+
 	err = cm.Delete()
 	require.NoError(t, err, "Unexpected error on Delete")
+}
+
+func TestCMLockLostV2(t *testing.T) {
+	fakeClient := fakek8sclient.NewSimpleClientset()
+	coreops.SetInstance(coreops.New(fakeClient))
+	cmIntf, err := New("px-configmaps-test", nil, testLockTimeout, testLockAttempts, testLockRefreshDuration, testLockTTL)
+	require.NoError(t, err, "Unexpected error on New")
+
+	cm := cmIntf.(*configMap)
+
+	id1 := "id1"
+	key1 := "key1"
+
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+
+	err = cm.PatchKeyLocked(false, id1, key1, "val1")
+	require.NoError(t, err, "Unexpected error in Patch")
+
+	// case: lock lost with NO new owner
+	setV2LockOwnerForTesting(t, cm, key1, "", time.Time{})
+	err = cm.PatchKeyLocked(false, id1, key1, "val2")
+	require.Error(t, err, "Expected error in Patch")
+	require.ErrorContains(t, err, "lock check failed")
+
+	// case: re-take the lock and update
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+
+	err = cm.PatchKeyLocked(false, id1, key1, "val1")
+	require.NoError(t, err, "Unexpected error in Patch")
+
+	// case: lock lost to a new owner
+	setV2LockOwnerForTesting(t, cm, key1, "new-owner", time.Now().Add(5*time.Minute))
+	err = cm.PatchKeyLocked(false, id1, key1, "val2")
+	require.Error(t, err, "Expected error in Patch")
+	require.ErrorContains(t, err, "lock check failed")
+
+	// case: re-taking the lock should fail with "configmap is locked" error
+	err = cm.LockWithKey(id1, key1)
+	require.Error(t, err, "Expected error in Patch")
+	require.ErrorContains(t, err, "ConfigMap is locked")
+
+	// case: new owner releases the lock; then we should be able to take the lock
+	setV2LockOwnerForTesting(t, cm, key1, "", time.Time{})
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+
+	err = cm.PatchKeyLocked(false, id1, key1, "val3")
+	require.NoError(t, err, "Unexpected error in Patch")
+
+	err = cm.UnlockWithKey(key1)
+	require.NoError(t, err, "Unexpected error in UnlockWithKey(key1)")
+
+	resultMap, err := cm.Get()
+	require.NoError(t, err, "Unexpected error in Get")
+	require.Contains(t, resultMap, key1)
+	require.Equal(t, "val3", resultMap[key1])
+
+	err = cm.Delete()
+	require.NoError(t, err, "Unexpected error on Delete")
+}
+
+func TestDeleteKeyLockedV2(t *testing.T) {
+	fakeClient := fakek8sclient.NewSimpleClientset()
+	coreops.SetInstance(coreops.New(fakeClient))
+	cmIntf, err := New("px-configmaps-test", nil, testLockTimeout, testLockAttempts, testLockRefreshDuration, testLockTTL)
+	require.NoError(t, err, "Unexpected error on New")
+
+	cm := cmIntf.(*configMap)
+
+	id1 := "id1"
+	key1 := "key1"
+
+	err = cm.LockWithKey(id1, key1)
+	require.NoError(t, err, "Unexpected error in LockWithKey(id1,key1)")
+
+	err = cm.PatchKeyLocked(false, id1, key1, "val1")
+	require.NoError(t, err, "Unexpected error in Patch")
+
+	resultMap, err := cm.Get()
+	require.NoError(t, err, "Unexpected error in Get")
+	require.Contains(t, resultMap, key1)
+	require.Equal(t, "val1", resultMap[key1])
+
+	err = cm.DeleteKeyLocked(false, id1, key1)
+	require.NoError(t, err, "Unexpected error in DeleteKeyLocked")
+
+	err = cm.UnlockWithKey(key1)
+	require.NoError(t, err, "Unexpected error in UnlockWithKey(key1)")
+
+	resultMap, err = cm.Get()
+	require.NoError(t, err, "Unexpected error in Get")
+	require.NotContains(t, resultMap, key1)
+
+	err = cm.Delete()
+	require.NoError(t, err, "Unexpected error on Delete")
+}
+
+func setV2LockOwnerForTesting(t *testing.T, cm *configMap, key, owner string, expiration time.Time) {
+	require.Eventually(t, func() bool {
+		rawCM, err := coreops.Instance().GetConfigMap(cm.name, k8sSystemNamespace)
+		if err != nil {
+			return false
+		}
+		lockOwners, lockExpirations, err := cm.parseLocks(rawCM)
+		if err != nil {
+			return false
+		}
+		if owner == "" {
+			delete(lockOwners, key)
+			delete(lockExpirations, key)
+		} else {
+			lockOwners[key] = owner
+			lockExpirations[key] = expiration
+		}
+
+		err = cm.generateConfigMapData(rawCM, lockOwners, lockExpirations)
+		if err != nil {
+			return false
+		}
+		_, err = coreops.Instance().UpdateConfigMap(rawCM)
+		return err == nil
+	}, 5*time.Second, 100*time.Millisecond)
 }
--- k8s/core/configmap/configmap_test.go
+++ k8s/core/configmap/configmap_test.go
@@ -17,7 +17,7 @@ func TestGetConfigMap(t *testing.T) {
 	configData := map[string]string{
 		"key1": "val1",
 	}
-	cm, err := New("px-configmaps-test", configData, lockTimeout, 5, 0, 0)
+	cm, err := New("px-configmaps-test", configData, testLockTimeout, 5, 0, 0)
 	require.NoError(t, err, "Unexpected error in creating configmap")
 
 	resultMap, err := cm.Get()
@@ -34,56 +34,53 @@ func TestDeleteConfigMap(t *testing.T) {
 		"key1": "val1",
 	}
 
-	cm, err := New("px-configmaps-test", configData, lockTimeout, 5, 0, 0)
+	cm, err := New("px-configmaps-test", configData, testLockTimeout, 5, 0, 0)
 	require.NoError(t, err, "Unexpected error in creating configmap")
 
 	err = cm.Delete()
 	require.NoError(t, err, "Unexpected error in delete")
-
 }
 
-func TestPatchConfigMap(t *testing.T) {
+func TestIncrementGeneration(t *testing.T) {
 	fakeClient := fakek8sclient.NewSimpleClientset()
 	coreops.SetInstance(coreops.New(fakeClient))
 
 	configData := map[string]string{
-		"key1": "val1",
+		"key1": "1",
 	}
-
-	cm, err := New("px-configmaps-test", configData, lockTimeout, 5, 0, 0)
+	cmIntf, err := New("px-configmaps-test", configData, testLockTimeout, 5, 0, 0)
 	require.NoError(t, err, "Unexpected error in creating configmap")
 
-	dummyData := map[string]string{
-		"key2": "val2",
-	}
+	cm := cmIntf.(*configMap)
 
-	err = cm.Patch(dummyData)
-	require.NoError(t, err, "Unexpected error in Patch")
-	resultMap, err := cm.Get()
-	require.Contains(t, resultMap, "key1")
-	require.Contains(t, resultMap, "key2")
-	fmt.Println(resultMap)
-}
+	rawCM, err := coreops.Instance().GetConfigMap(cm.name, k8sSystemNamespace)
+	require.NoError(t, err, "Unexpected error in getting raw configmap")
 
-func TestUpdateConfigMap(t *testing.T) {
-	fakeClient := fakek8sclient.NewSimpleClientset()
-	coreops.SetInstance(coreops.New(fakeClient))
+	require.Equal(t, "", rawCM.Data[pxGenerationKey])
+	newGen := cm.incrementGeneration(rawCM)
+	require.Equal(t, "1", rawCM.Data[pxGenerationKey])
+	require.Equal(t, uint64(1), newGen)
 
-	configData := map[string]string{
-		"key1": "val1",
-	}
+	newGen = cm.incrementGeneration(rawCM)
+	require.Equal(t, "2", rawCM.Data[pxGenerationKey])
+	require.Equal(t, uint64(2), newGen)
 
-	cm, err := New("px-configmaps-test", configData, lockTimeout, 5, 0, 0)
-	require.NoError(t, err, "Unexpected error in creating configmap")
+	rawCM.Data[pxGenerationKey] = "123456789"
+	newGen = cm.incrementGeneration(rawCM)
+	require.Equal(t, "123456790", rawCM.Data[pxGenerationKey])
+	require.Equal(t, uint64(123456790), newGen)
 
-	dummyData := map[string]string{
-		"key2": "val2",
-	}
+	rawCM.Data[pxGenerationKey] = "invalid"
+	newGen = cm.incrementGeneration(rawCM)
+	require.Equal(t, "1", rawCM.Data[pxGenerationKey])
+	require.Equal(t, uint64(1), newGen)
 
-	err = cm.Update(dummyData)
-	require.NoError(t, err, "Unexpected error in Update")
-	resultMap, err := cm.Get()
-	require.NotContains(t, resultMap, "key1")
-	require.Contains(t, resultMap, "key2")
-	fmt.Println(resultMap)
+	// max uint64
+	rawCM.Data[pxGenerationKey] = "18446744073709551615"
+	newGen = cm.incrementGeneration(rawCM)
+	require.Equal(t, "1", rawCM.Data[pxGenerationKey])
+	require.Equal(t, uint64(1), newGen)
+
+	err = cm.Delete()
+	require.NoError(t, err, "Unexpected error in delete")
 }
